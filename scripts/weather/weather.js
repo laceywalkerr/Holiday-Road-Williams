b34ada0b19ab3f74217a7f6b52e20474e5a4fbb3
Lacey Walker <65372269+laceywalkerr@users.noreply.github.com>
2020-06-11 14:54:10 -0500

    brenda helped me and now it works

--- scripts/weather/weather.js
+++ scripts/weather/weather.js
@@ -1,6 +1,15 @@
 const weatherConverter = (weatherObject) => {
+    console.log(weatherObject)
     const weatherHTMLRepresentation = 
-        `<option value="#">${weatherObject.fullName}</option>`
+        `
+        <h1>hi</h1>
+        <section> 
+            <div>Date: ${weatherObject.main.dt_text}</div>
+            <div>City: ${weatherObject.main.name}</div>
+            <div>Highest Temp: ${weatherObject.main.temp_max}</div>
+            <div>Lowest Temp: ${weatherObject.main.temp_min}</div>
+            <div>Humidity: ${weatherObject.main.humidity}</div>
+        </section>`
 
     return weatherHTMLRepresentation
 }
