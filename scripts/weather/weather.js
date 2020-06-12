e3f5aa5016ec28b8855fcfb3ab9207c42ab5a972
Lacey Walker <65372269+laceywalkerr@users.noreply.github.com>
2020-06-12 09:47:11 -0500

    fixed typo

--- scripts/weather/weather.js
+++ scripts/weather/weather.js
@@ -8,7 +8,7 @@ const weatherConverter = (weatherObject) => {
             <div>Weather: ${weatherObject.weather[0].description}</div>
             <div>Current Temp: ${weatherObject.main.temp}&deg;</div>
             <div>Feels Like: ${weatherObject.main.feels_like}&deg;</div>
-            <div>High / Low Temp: ${weatherObject.main.temp_max}&deg;</div>
+            <div>Highest Temp: ${weatherObject.main.temp_max}&deg;</div>
             <div>Lowest Temp: ${weatherObject.main.temp_min}&deg;</div>
             <div>Humidity: ${weatherObject.main.humidity}</div>
             <br> 
