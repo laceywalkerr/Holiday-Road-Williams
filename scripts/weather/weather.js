71148d9897e637ff4df02e82d5382851bdf3ffa8
Lacey Walker <65372269+laceywalkerr@users.noreply.github.com>
2020-06-12 10:23:47 -0500

    added percent symbol to humidity

--- scripts/weather/weather.js
+++ scripts/weather/weather.js
@@ -10,7 +10,7 @@ const weatherConverter = (weatherObject) => {
             <div>Feels Like: ${weatherObject.main.feels_like}&deg;</div>
             <div>Highest Temp: ${weatherObject.main.temp_max}&deg;</div>
             <div>Lowest Temp: ${weatherObject.main.temp_min}&deg;</div>
-            <div>Humidity: ${weatherObject.main.humidity}</div>
+            <div>Humidity: ${weatherObject.main.humidity}&#37;</div>
             <br> 
         </section>`
 
