6990b533b0d2fc190f8ce8b0de1ff10f324641f5
Lacey Walker <65372269+laceywalkerr@users.noreply.github.com>
2020-06-11 20:07:27 -0500

    made minor changes

--- scripts/weather/weather.js
+++ scripts/weather/weather.js
@@ -3,12 +3,12 @@ const weatherConverter = (weatherObject) => {
     const weatherHTMLRepresentation = 
         `
         <section> 
-            <h2></h2>
-            <div>Date: ${weatherObject.main.dt_text}</div>
-            <div>City: ${weatherObject.main.name}</div>
+            <h3>Nashville Weather</h3>
+            <div>Weather: ${weatherObject.weather[0].description}</div>
             <div>Highest Temp: ${weatherObject.main.temp_max}&deg;</div>
             <div>Lowest Temp: ${weatherObject.main.temp_min}&deg;</div>
             <div>Humidity: ${weatherObject.main.humidity}</div>
+
         </section>`
 
     return weatherHTMLRepresentation
--- scripts/weather/weatherData.js
+++ scripts/weather/weatherData.js
@@ -1,7 +1,7 @@
 let weatherCollection = []
 
 const getWeatherData = () => {
-    return fetch(`http://api.openweathermap.org/data/2.5/forecast?zip=37174&units=Imperial&cnt=3&APPID=${API.weatherKey}`).then(
+    return fetch(`http://api.openweathermap.org/data/2.5/forecast?zip=37211&units=Imperial&cnt=1&APPID=${API.weatherKey}`).then(
         (response) => {
             return response.json()
         }
