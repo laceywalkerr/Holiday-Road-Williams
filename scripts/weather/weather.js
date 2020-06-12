bc65f7260db14900e0db72b34c8d558819be61ac
Lacey Walker <65372269+laceywalkerr@users.noreply.github.com>
2020-06-12 09:36:38 -0500

    minor changes, added feels like to weather

--- scripts/weather/weather.js
+++ scripts/weather/weather.js
@@ -4,11 +4,14 @@ const weatherConverter = (weatherObject) => {
         `
         <section> 
             <h3>Nashville Weather</h3>
+            <h4>Today</h4>
             <div>Weather: ${weatherObject.weather[0].description}</div>
-            <div>Highest Temp: ${weatherObject.main.temp_max}&deg;</div>
+            <div>Current Temp: ${weatherObject.main.temp}&deg;</div>
+            <div>Feels Like: ${weatherObject.main.feels_like}&deg;</div>
+            <div>High / Low Temp: ${weatherObject.main.temp_max}&deg;</div>
             <div>Lowest Temp: ${weatherObject.main.temp_min}&deg;</div>
             <div>Humidity: ${weatherObject.main.humidity}</div>
-
+            <br>
         </section>`
 
     return weatherHTMLRepresentation
